f441ef1f3fed30d6d7ce75ddfb508ce7ef6cb1fc
Ian Hawke <I.Hawke@soton.ac.uk>
2018-05-08 15:26:04 +0100

    Ensure pytest installed

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,8 @@ set -e
 
 ##### Install specific package versions with pip #####
 
+pip3 install pytest
+
 # pip3 install pandas==0.22.0
 # pip3 install matplotlib==2.1.2
 # pip3 install scikit-learn==0.19.1
